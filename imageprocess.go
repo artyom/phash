50f869730991670c1d821282fc1d998ca927a3e7
Artyom Pervukhin <github@artyom.dev>
2020-08-04 21:40:12 +0300

    Reduce allocations inside getImageMatrix
    name         old time/op    new time/op    delta
    ImageHash-8    33.6ms ± 5%    34.1ms ± 3%     ~     (p=0.447 n=10+9)
    
    name         old alloc/op   new alloc/op   delta
    ImageHash-8    25.2kB ± 0%    21.1kB ± 0%  -16.29%  (p=0.000 n=10+9)
    
    name         old allocs/op  new allocs/op  delta
    ImageHash-8     1.16k ± 0%     0.14k ± 0%  -88.12%  (p=0.000 n=10+10)

--- imageprocess.go
+++ imageprocess.go
@@ -10,17 +10,26 @@ func getImageMatrix(img image.Image) [][]float64 {
 
 	vals := make([][]float64, xSize)
 
+	var getXYValue func(x, y int) float64
+	switch img2 := img.(type) {
+	case *image.NRGBA:
+		getXYValue = func(x, y int) float64 {
+			_, _, b, _ := img2.NRGBAAt(x, y).RGBA()
+			return float64(b)
+		}
+	default:
+		getXYValue = func(x, y int) float64 {
+			_, _, b, _ := img.At(x, y).RGBA()
+			return float64(b)
+		}
+	}
+
 	for x := 0; x < xSize; x++ {
 		vals[x] = make([]float64, ySize)
 		for y := 0; y < ySize; y++ {
-			vals[x][y] = getXYValue(img, x, y)
+			vals[x][y] = getXYValue(x, y)
 		}
 	}
 
 	return vals
-}
-
-func getXYValue(img image.Image, x int, y int) float64 {
-	_, _, b, _ := img.At(x, y).RGBA()
-	return float64(b)
 }
